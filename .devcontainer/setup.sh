abb16aea4de0b73bfa1b71f014fd5031490314cc
Israel Perales <israel-perales@outlook.com>
2021-11-04 04:25:28 +0000

    add yarn install

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -22,3 +22,4 @@ cp -R /root/.oh-my-zsh /home/$USERNAME
 cp /root/.zshrc /home/$USERNAME
 sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
 chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
+yarn install
